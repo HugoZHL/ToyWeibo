25d6a46684a413d4b51432935f034ac418f9e889
hjhhsy120 <hjhhsy120@163.com>
2021-01-16 10:07:41 -0800

    runnable ver1 with a few bugs

--- backend.py
+++ backend.py
@@ -1,8 +1,8 @@
 
-# sudo bin/gbuild small ???/small.nt
-# sudo bin/ghttp 9000 small
+# sudo bin/gbuild toyweibo ../ToyWeibo/sql2rdf/small.nt
+# sudo bin/ghttp 9000 toyweibo
 # sudo bin/shutdown 9000
-# sudo bin/gdrop small
+# sudo bin/gdrop toyweibo
 
 import GstoreConnector
 import json
@@ -14,24 +14,30 @@ prefix = {'u':'http://example.org/user/',
     'wa': 'http://example.org/weibo_attr/',
     'r': 'http://example.org/rel/'}
 
-database = 'small'
+database = 'toyweibo'
 u_len = len(prefix['u'])
 w_len = len(prefix['w'])
+prefix_string = ''.join(['prefix %s:<%s> ' % (k,v) for k,v in prefix.items()])
 gc = GstoreConnector.GstoreConnector('localhost', 9000, 'root', '123456')
 _ = gc.load(database)
 
-def transfer(l):
-    ls = l.split(' ')
-    for lls in ls:
-        for k,v in prefix.items():
-            if lls.find(k+':') == 0:
-                llss = lls.split(':')
-                uri = '<' + v + llss[1] + '>'
-                l = l.replace(lls, uri)
-    return l
+# def transfer(l):
+#     ls = l.split(' ')
+#     for lls in ls:
+#         for k,v in prefix.items():
+#             if lls.startswith(k+':'):
+#                 llss = lls.split(':')
+#                 uri = '<' + v + llss[1] + '>'
+#                 l = l.replace(lls, uri)
+#     return l
+
+# basic functions
 
 def run_sparql(l):
-    return gc.query(database, 'json', transfer(l))
+    ret = gc.query(database, 'json', prefix_string + l)
+    if not l.startswith('select'):
+        gc.checkpoint(database)
+    return ret
 
 def query(l):
     return json.loads(run_sparql(l))['results']['bindings']
@@ -43,12 +49,18 @@ def is_unique(key, value):
     else:
         return False
 
+
+
+# user queries
+
+def user_query(uid, k):
+    return query('select ?x where { u:%s ua:%s ?x }' % (uid, k))
+
+def user_query_value(uid, k):
+    return user_query(uid, k)[0]['x']['value']
+
 def user_query_values(uid, keys):
-    ans = []
-    for k in keys:
-        result = query('select ?x where { u:%s ua:%s ?x }' % (uid, k))[0]['x']['value']
-        ans.append(result)
-    return ans
+    return [user_query_value(uid, k) for k in keys]
 
 def user_insert_strings(uid, kv_dict):
     for k,v in kv_dict.items():
@@ -60,13 +72,17 @@ def user_insert_ints(uid, kv_dict):
 
 def user_delete_values(uid, keys):
     for k in keys:
-        _ = run_sparql('delete { ?uid ?k ?v } where { ?uid ?k ?v filter( ?uid = u:%s ) filter( ?k = ua:%s ) } ' % (uid, k))
+        _ = run_sparql('delete { ?uid ?k ?v } where { ?uid ?k ?v filter( ?uid = u:%s && ?k = ua:%s ) } ' % (uid, k))
 
 def user_update_int(uid, k, delta):
-    v = int(user_query_values(uid, [k])[0])
+    v = int(user_query_value(uid, k)[0])
     v += delta
     user_delete_values(uid, [k])
     user_insert_ints(uid, {k:v})
+
+
+
+# weibo queries
 
 def weibo_query_values(wid, keys):
     ans = []
@@ -81,13 +97,17 @@ def weibo_insert_ints(wid, kv_dict):
 
 def weibo_delete_values(wid, keys):
     for k in keys:
-        _ = run_sparql('delete { ?wid ?k ?v } where { ?wid ?k ?v filter( ?wid = w:%s ) filter( ?k = wa:%s ) } ' % (uid, k))
+        _ = run_sparql('delete { ?wid ?k ?v } where { ?wid ?k ?v filter( ?wid = w:%s && ?k = wa:%s ) } ' % (wid, k))
 
 def weibo_update_int(wid, k, delta):
     v = int(weibo_query_values(wid, [k])[0])
     v += delta
     weibo_delete_values(wid, [k])
     weibo_insert_ints(wid, {k:v})
+
+
+
+# user account management
 
 def handle_bad(ls):
     return [l.replace('\\', '\\\\').replace('\"', '\\\"') for l in ls]
@@ -116,7 +136,7 @@ def login(email, password):
         uid = uids[0]['uid']['value'][u_len:]
         return (True, uid)
 
-def update_info(uid, email, screen_name, location, url, gender):
+def update_info0(uid, email, screen_name, location, url, gender):
     email, screen_name, location, url, gender = handle_bad([email, screen_name, location, url, gender])
     cur_email, cur_screen_name = user_query_values(uid, ['email', 'screen_name'])
     if not email == cur_email and not is_unique('email', email):
@@ -129,16 +149,36 @@ def update_info(uid, email, screen_name, location, url, gender):
 
 def change_password(uid, old_pwd, new_pwd):
     old_pwd, new_pwd = handle_bad([old_pwd, new_pwd])
-    if old_pwd != user_query_values(uid, ['password'])[0]:
+    if old_pwd != user_query_value(uid, 'password')[0]:
         return False
     else:
         user_delete_values(uid, ['password'])
         user_insert_strings(uid, {'password': new_pwd})
         return True
 
+def update_info(uid, email, screen_name, password, location, url, gender):
+    email, screen_name, password, location, url, gender = handle_bad([email, screen_name, password, location, url, gender])
+    cur_email, cur_screen_name = user_query_values(uid, ['email', 'screen_name'])
+    if not email == cur_email and not is_unique('email', email):
+        return (False, 'Email already exists')
+    if not screen_name == cur_screen_name and not is_unique('screen_name', screen_name):
+        return (False, 'Screen name already exists')
+    user_delete_values(uid, ['email', 'screen_name', 'password', 'location', 'url', 'gender'])
+    user_insert_strings(uid, {'email':email, 'screen_name':screen_name, 'password':password, 'location':location, 'url':url, 'gender':gender})
+    return (True, 'Succeeded')
+
+
+# weibo management except delete (in others)
+
+def weibo_exists(wid):
+    return len(query('select ?d where { w:%s wa:date ?d }' % (wid))) != 0
+
+def weibo_uid(wid):
+    return query('select ?uid where { w:%s r:by ?uid }' % (wid))[0]['uid']['value'][u_len:]
+
 def weibo_info(wid):
-    uid = query('select ?uid where { w:%s r:by ?uid }' % (wid))[0]['uid']['value'][u_len:]
-    return [wid, uid] + weibo_query_values(wid, ['text', 'date']) # TODO what to show?
+    #return [wid, weibo_uid(wid)] + weibo_query_values(wid, ['text', 'date', 'attitudesnum'])
+    return wid
 
 def latest_weibo(uid):
     ans = []
@@ -164,17 +204,23 @@ def user_weibo(uid):
         ans.append(weibo_info(wid))
     return ans
     
-def send_weibo(uid, text):
-    text = handle_bad([text])[0]
+def send_weibo(uid, text, topic):
+    text, topic = handle_bad([text, topic])
     wid = query('select ?wid where { w:next r:is ?wid }')[0]['wid']['value']
     next_wid = str(int(wid) + 1)
     _ = run_sparql('delete data { w:next r:is \"%s\" }' % (wid))
     _ = run_sparql('insert data { w:next r:is \"%s\" }' % (next_wid))
     _ = run_sparql('insert data { w:%s wa:text \"%s\" }' % (wid, text))
+    _ = run_sparql('insert data { w:%s wa:topic \"%s\" }' % (wid, topic))
     _ = run_sparql('insert data { w:%s r:by u:%s }' % (wid, uid))
-    weibo_insert_ints(wid, {'repostsnum':0, 'commentsnum':0})
     _ = run_sparql('insert data { w:%s wa:date \"%s\" }' % (wid, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    weibo_insert_ints(wid, {'repostsnum':0, 'commentsnum':0, 'attitudesnum':0})
     user_update_int(uid, 'statusesnum', 1)
+
+
+
+
+# user relations
 
 def find_user(screen_name):
     uid_urls = query('select ?uid where { ?uid ua:screen_name \"%s\" }' % (screen_name))
@@ -202,7 +248,10 @@ def unfollow(uid1, uid2):
     user_update_int(uid2, 'followersnum', -1)
 
 def is_following(uid1, uid2):
-    return len(query('select ?r where { u:%s ?r u:%s }' % (uid1, uid2))) != 0
+    return len(query('select ?r where { u:%s ?r u:%s filter( ?r = r:follow ) }' % (uid1, uid2))) != 0
+
+def is_following_list(uid1, uid2s):
+    return [is_following(uid1, uid2) for uid2 in uid2s]
 
 def routes(uid1, uid2, cnt):
     q = 'select'
@@ -227,5 +276,59 @@ def routes4(uid1, uid2):
     if is_following(uid1, uid2):
         ans.append([uid1, uid2])
     for i in range(3):
-        ans += routes(uid1, uid2, i+1)
+        result = routes(uid1, uid2, i+1)
+        for r in result:
+            if len(set(r)) == len(r):
+                ans.append(r)
     return ans
+
+def nhops(uid, n):
+    q = 'select ?x%d where { u:%s' % (n-1, uid)
+    for i in range(n-1):
+        q += ' r:follow ?x%d . ?x%d' % (i, i)
+    q += ' r:follow ?x%d }' % (n-1)
+    data = query(q)
+    return [d['x%d' % (n-1)]['value'][u_len:] for d in data]
+
+def nhops_all(uid, n):
+    ans = set()
+    for i in range(n):
+        ans.update(nhops(uid, i+1))
+    return list(ans)
+
+
+
+# like
+
+def is_liked_by(wid, uid):
+    return len(query('select ?r where { w:%s ?r u:%s filter( ?r = r:likedby ) }' % (wid, uid))) != 0
+
+def like_it(wid, uid):
+    _ = run_sparql('insert data { w:%s r:likedby u:%s }' % (wid, uid))
+    weibo_update_int(wid, 'attitudesnum', 1)
+
+def cancel_like_it(wid, uid):
+    _ = run_sparql('delete data { w:%s r:likedby u:%s }' % (wid, uid))
+    weibo_update_int(wid, 'attitudesnum', -1)
+
+
+
+
+
+# others
+
+def delete_weibo(wid):
+    uid = weibo_uid(wid)
+    user_update_int(uid, 'statusesnum', -1)
+    _ = run_sparql('delete { ?x ?y ?z } where { ?x ?y ?z filter( ?x = w:%s || ?z = w:%s ) }' % (wid, wid))
+
+def bunch_register(n):
+    for i in range(n):
+        x = str(i+1)
+        register(x,x,x,'','','')
+
+def bunch_follow(n):
+    for i in range(n-1):
+        follow(str(i+1), str(i+2))
+
+
--- router_backup.py
+++ router_backup.py
@@ -1,56 +1,7 @@
 from flask import *
-from structs import *
-import backend as bc
+from utils import *
 
 app = Flask(__name__)
-
-def genUserInfo(uid):
-    uid = str(uid)
-    r = bc.user_query_values(uid, ['screen_name', 'location', 'gender', 'followersnum', 'friendsnum', 'statusesnum', 'created_at'])
-    infos = {
-        'userID': int(uid),
-        'name': r[0],
-        'location': r[1],
-        'gender': r[2],
-        'followersum': int(r[3]),
-        'friendsum': int(r[4]),
-        'statusesum': int(r[5]),
-        'created_at': r[6],
-    }
-    return infos
-
-def genUserInfo2(infos, myuid):
-    myuid = str(myuid)
-    uid = str(infos['userID'])
-    infos['following'] = bc.is_following(myuid, uid)
-    infos['ismyself'] = (myuid == uid)
-    return infos
-
-def genUserInfoBad(uid):
-    uid = str(uid)
-    r = bc.user_query_values(uid, ['screen_name', 'email', 'location', 'gender'])
-    infos = {
-        'name': r[0],
-        'email': r[1],
-        'province': '',
-        'city': '',
-        'location': r[2],
-        'gender': r[3],
-    }
-    return infos
-
-def genWeibo(wid, myuid):
-    wid = str(wid)
-    myuid = str(myuid)
-    uid = bc.weibo_uid(wid)
-    username = bc.user_query_value(uid, 'screen_name')
-    r = bc.weibo_query_values(wid, ['text', 'date', 'repostsnum', 'commentsnum', 'attitudesnum', 'topic'])
-    # (self, userID, postID, username, text, time, repostsum, commentsum, attitudesum, topic, replies)
-    # Weibo(0, 1, 'test1', '今天天气真好', '2020年1月1日13:45', 1, 2, 3, '天气', [])
-    wb = Weibo(int(uid), int(wid), username, r[0], r[1], int(r[2]), int(r[3]), int(r[4]), r[5], [])
-    wb.myself = (myuid == uid)
-    wb.praised = bc.is_liked_by(wid, myuid)
-    return wb
 
 @app.route("/")
 def homepage():
@@ -61,33 +12,27 @@ def homepage():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
-    info = None
+    error = None
     if request.method == 'POST':
-        ok, info = bc.login(request.form['email'], request.form['password'])
-        if ok:
-            username = bc.user_query_value(info, 'screen_name')
+        userID, username, error = valid_login(request.form['email'], request.form['password'])
+        if not error:
             resp = make_response(redirect('/square'))
             resp.set_cookie('username', username, max_age=3600)
-            resp.set_cookie('userID', info, max_age=3600)
+            resp.set_cookie('userID', str(userID), max_age=3600)
             return resp
-    return render_template("login.html", error=info)
+    return render_template("login.html", error=error)
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
-    info = None
+    error = None
     if request.method == 'POST':
-        pwd = request.form['password']
-        if pwd != request.form['rpassword']:
-            ok = False
-            info = 'Password inconsistent'
-        else:
-            ok, info = bc.register(request.form['email'], request.form['username'], pwd, '', '', '')
-        if ok:
+        userID, error = valid_register(request.form['email'], request.form['username'], request.form['password'], request.form['rpassword'])
+        if not error:
             resp = make_response(redirect('/square'))
             resp.set_cookie('username', request.form['username'], max_age=3600)
-            resp.set_cookie('userID', info, max_age=3600)
+            resp.set_cookie('userID', str(userID), max_age=3600)
             return resp
-    return render_template("register.html", error=info)
+    return render_template("register.html", error=error)
 
 
 @app.route("/profile", methods=['POST', 'GET'])
@@ -98,38 +43,33 @@ def show_profile(userID=0):
             searching = request.form['searching']
             return redirect('/searchresult/%s' % searching)
         if userID == 0: userID = int(request.cookies['userID'])
-        myuid = request.cookies['userID']
-        myself = (userID == int(myuid))
-        follow = bc.is_following(str(myuid), str(userID))
-        infos = genUserInfo(userID)
-        posts = [genWeibo(wid, myuid) for wid in bc.user_weibo(str(userID))]
+        myself = (userID == int(request.cookies['userID']))
+        follow = False
+        infos = get_weibo_info_from_userid(userID)
+        posts = get_posts_from_userid(userID)
+        posts = get_praise_infos(userID, posts)
         username = request.cookies['username']
         return render_template("profile.html", username=username, myself=myself, follow=follow, infos=infos, posts=posts, title='个人主页')
     except KeyError:
         print(KeyError, " keyerror for username")
         return redirect('/')
 
+
 @app.route("/edit_profile", methods=['POST','GET'])
 def edit_profile():
+    error = None
     try:
-        info = None
         userID = int(request.cookies["userID"])
-        infos = genUserInfoBad(userID)
+        infos = get_edit_info_from_userid(userID)
         if request.method == 'POST':
-            pwd = request.form['password']
-            if pwd != request.form['rpassword']:
-                ok = False
-                info = 'Password inconsistent'
-            else:
-                # uid, email, screen_name, password, location, url, gender
-                ok, info = bc.update_info(str(userID), request['email'], request['name'], pwd, request['location'], '', request['gender'])
-            if not ok:
-                return render_template("edit_profile.html", infos=infos, error=info)
+            error = update_infos(userID, request)
+            if error:
+                return render_template("edit_profile.html", infos=infos, error=error)
             else:
                 resp = make_response(redirect('/profile/%s' % userID))
                 resp.set_cookie('username', request.form['name'], max_age=3600)
                 return resp
-        return render_template("edit_profile.html", infos=infos, error=info)
+        return render_template("edit_profile.html", infos=infos, error=error)
     except KeyError:
         print(KeyError, " keyerror for username")
         return redirect('/')
@@ -141,12 +81,10 @@ def result_page(searching=""):
     if not searching or request.method=='POST':
         searching = request.form['searching']
     try:
-        myUserID = str(request.cookies['userID'])
+        myUserID = int(request.cookies['userID'])
         username = request.cookies["username"]
-        ok, info = bc.find_user(searching)
-        results = []
-        if ok:
-            results = [genUserInfo2(genUserInfo(info), myUserID)]
+        results = search_user_in_db(searching)
+        results = get_follow_status(myUserID, results)
         return render_template("searchresult.html", username=username, searching=searching, users=results, title='搜索结果')
     except KeyError:
         print(KeyError, " keyerror for username")
@@ -165,9 +103,10 @@ def show_following(userID=0):
             if userID == 0:
                 userID = myUserID
             myself = (userID == myUserID)
-            follow = bc.is_following(str(myUserID), str(userID))
-            infos = genUserInfo(userID)
-            followings = [genUserInfo2(genUserInfo(uid), myUserID) for uid in bc.followings(str(userID))]
+            follow = False
+            infos = get_weibo_info_from_userid(userID)
+            followings = get_following_from_userid(userID)
+            followings = get_follow_status(myUserID, followings)
             username = request.cookies['username']
             return render_template("followings.html", username=username, myself=myself, follow=follow, infos=infos, users=followings, title='关注列表')
     except KeyError:
@@ -187,9 +126,10 @@ def show_follower(userID=0):
             if userID == 0:
                 userID = myUserID
             myself = (userID == myUserID)
-            follow = bc.is_following(str(myUserID), str(userID))
-            infos = genUserInfo(userID)
-            followers = [genUserInfo2(genUserInfo(uid), myUserID) for uid in bc.followers(str(userID))]
+            follow = True
+            infos = get_weibo_info_from_userid(userID)
+            followers = get_follower_from_userid(userID)
+            followers = get_follow_status(myUserID, followers)
             username = request.cookies['username']
             return render_template("followers.html", username=username, myself=myself, follow=follow, infos=infos, users=followers, title='粉丝列表')
     except KeyError:
@@ -206,7 +146,8 @@ def square():
         else:
             userID = int(request.cookies['userID'])
             username = request.cookies["username"]
-            posts = [genWeibo(wid, userID) for wid in bc.latest_weibo(str(userID))]
+            posts = get_all_posts_from_userid(userID)
+            posts = get_praise_infos(userID, posts)
             print(posts)
             return render_template("square.html", username=username, posts=posts, title='广场大厅')
     except KeyError:
@@ -223,7 +164,8 @@ def recommends():
         else:
             userID = int(request.cookies['userID'])
             username = request.cookies["username"]
-            posts = [genWeibo(wid, userID) for wid in bc.all_weibo()]
+            posts = get_all_host_posts(userID)
+            posts = get_praise_infos(userID, posts)
             print(posts)
             return render_template("recommends.html", username=username, posts=posts, title='热点推荐')
     except KeyError:
@@ -238,22 +180,11 @@ def adsearch():
         userID = int(request.cookies["userID"])
         username = request.cookies["username"]
         if request.method == 'POST':
-            ok1, uid1 = bc.find_user(request.form['email1'])
-            ok2, uid2 = bc.find_user(request.form['email2'])
-            if not ok1 or not ok2:
-                return render_template("adsearch.html", error='User not exist')
-            result = bc.routes4(uid1, uid2)
-            infos = {}
-            info_keys = set()
-            for r in result:
-                cnt = len(r)
-                re = tuple([genUserInfo(uid) for uid in r])
-                if cnt in info_keys:
-                    infos[cnt].append(re)
-                else:
-                    info_keys.add(cnt)
-                    infos[cnt] = [re]
-            return render_template("adsearchresult.html", title='高级搜索结果', username=username, infos=infos)
+            infos, error = get_adsearch_result(userID, request)
+            if error:
+                return render_template("adsearch.html", error=error)
+            else:
+                return render_template("adsearchresult.html", title='高级搜索结果', username=username, infos=infos)
         return render_template("adsearch.html", error=error)
     except KeyError:
         print(KeyError, " keyerror for username")
@@ -264,10 +195,6 @@ def adsearch():
 def follow():
     follow_id = request.form['follow']
     print('follow id:', follow_id)
-    uid = request.cookies["userID"]
-    follow_id = str(follow_id)
-    if not bc.is_following(uid, follow_id):
-        bc.follow(uid, follow_id)
     return redirect(request.form['path'])
 
 
@@ -275,21 +202,12 @@ def follow():
 def unfollow():
     unfollow_id = request.form['unfollow']
     print('unfollow id:', unfollow_id)
-    uid = request.cookies["userID"]
-    unfollow_id = str(unfollow_id)
-    if bc.is_following(uid, unfollow_id):
-        bc.unfollow(uid, unfollow_id)
     return redirect(request.form['path'])
 
 
 @app.route('/delete_post', methods=['POST'])
 def delete_post():
-    wid = request.form['postID']
-    print('delete post', wid)
-    wid = str(wid)
-    if bc.weibo_exists(wid):
-        print('I am trying to delete it...')
-        bc.delete_weibo(wid)
+    print('delete post', request.form['postID'])
     return redirect(request.form['path'])
 
 
@@ -297,36 +215,18 @@ def delete_post():
 def add_post():
     text = request.form['content']
     print('Add post: ', text)
-    topic = ''
-    if text[0] == '#':
-        pos = text.find('#', 1)
-        if pos > 0:
-            topic = text[1:pos]
-            text = text[pos+1:]
-    uid = request.cookies["userID"]
-    bc.send_weibo(uid, text, topic)
     return redirect(request.form['path'])
 
 
 @app.route('/add_praise', methods=['POST'])
 def add_praise():
-    wid = request.form['postID']
-    print('add praise', wid)
-    uid = request.cookies["userID"]
-    wid = str(wid)
-    if not bc.is_liked_by(wid, uid):
-        bc.like_it(wid, uid)
+    print('add praise', request.form['postID'])
     return redirect(request.form['path'])
 
 
 @app.route('/delete_praise', methods=['POST'])
 def delete_praise():
-    wid = request.form['postID']
-    print('delete praise', wid)
-    uid = request.cookies["userID"]
-    wid = str(wid)
-    if bc.is_liked_by(wid, uid):
-        bc.cancel_like_it(wid, uid)
+    print('delete praise', request.form['postID'])
     return redirect(request.form['path'])
 
 
