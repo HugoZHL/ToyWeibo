044922c5bdc38cb63732639873a2b3af30f705e0
张海林 <hugoz@192.168.0.100>
2021-01-17 10:48:01 +0800

    support forward(repost)

--- structs.py
+++ structs.py
@@ -13,9 +13,11 @@ class Weibo(object):
         'topic',
         'replies',
         'myself',
+        'forwardlist', # 所有转发者的三元信息(userID, username, text)，包括自己；如果有转发，那么Weibo的username是原微博的username，其他的仍是本微博信息。
+        'forwardshow', # 转发的时候显示的信息，即所有转发内容的拼合，e.g. "test1：好 // @test2：不好 //"
         'praised',
     ]
-    def __init__(self, userID, postID, username, text, time, repostsum, commentsum, attitudesum, topic, replies):
+    def __init__(self, userID, postID, username, text, time, repostsum, commentsum, attitudesum, topic, forwardlist, forwardshow, replies):
         self.userID = userID
         self.postID = postID
         self.username = username
@@ -25,6 +27,8 @@ class Weibo(object):
         self.commentsum = commentsum
         self.attitudesum = attitudesum
         self.topic = topic
+        self.forwardlist = forwardlist
+        self.forwardshow = forwardshow
         self.replies = replies
 
 
